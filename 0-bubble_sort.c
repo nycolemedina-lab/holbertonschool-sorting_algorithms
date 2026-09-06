dc67796a167fde9fafac49116ea9a65449b7a941
nycolemedina-lab <nycole.medina@gmain.com>
2026-09-06 16:45:06 -0400

    add task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+/**
+* bubble_sort - sorts an array if integers in ascending order
+*		using the bubble sort algorithm
+* @array: pointer to the first element of the array
+* @size: number of elements in the array
+* Return: void
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for(j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+this is readme for sorting_algorithms repo
